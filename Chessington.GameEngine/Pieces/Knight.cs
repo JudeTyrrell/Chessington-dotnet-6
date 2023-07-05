7fe459ca57bc5cd5942fdf815edfbc446232d118
Jude Tyrrell <Jude.Tyrrell@softwire.com>
2023-07-05 16:34:20 +0100

    no redundancy

--- Chessington.GameEngine/Pieces/Knight.cs
+++ Chessington.GameEngine/Pieces/Knight.cs
@@ -19,12 +19,7 @@ namespace Chessington.GameEngine.Pieces
             {
                 for (int j = -2; j < 3; j++)
                 {
-                    if (Math.Abs(i) == Math.Abs(j) || i == 0 || j == 0)
-                    {
-                        continue;
-                    }
-                    else
-                    {
+                    if (Math.Abs(i) != Math.Abs(j) && i != 0 && j != 0) {
                         if (!IsOccupied(board, position.Row + i, position.Col + j))
                         {
                             moves.Add(new Square(position.Row + i, position.Col + j));
