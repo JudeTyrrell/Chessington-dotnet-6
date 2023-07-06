033cd3da989eb198bd30c01f261e5065826dc176
Jude Tyrrell <Jude.Tyrrell@softwire.com>
2023-07-06 15:44:50 +0100

    Added Board interface.

---
+++ Chessington.GameEngine/IBoard.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Chessington.GameEngine.Pieces;
+
+namespace Chessington.GameEngine;
+
+public interface IBoard
+{
+    public IList<Piece> CapturedPieces { get; }
+}
